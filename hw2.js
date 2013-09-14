126757d49e1579e510885f1516a9406093ed70e8
chengjason <jcheng19@montgomerycollege.edu>
2013-09-14 16:49:12 -0400

    Update hw2.js

--- hw2.js
+++ hw2.js
@@ -15,7 +15,8 @@
  */
 
 // Problem 1) Using JavaScript comments, write your name and today's date below
-
+//My Name: Jason Cheng  
+//Today's Date: September 14, 2013
 
 // Problem 2) Create separate variables to store the following. 
 // Send all variables to the console.
@@ -27,14 +28,29 @@
 	// The total cost of a 10 dollar book plus 5% sales tax
 	// Whether or not the light switch is on
 	// Whether or not Superman can fly
-
+var myEmailAdd ='cadillacboy09@gmail.com';
+var myMajor = 'Computer Applications';
+var myTextEditor = 'Dreamweaver CS6';
+var Zip = '20850';
+var pi = 3.14159;
+var x = 10, bookCost = x*(1+0.05);
+var lightSwitchOn = true, lightSwitchOff = false;
+var superManFly = true; 
+console.log(myEmailAdd, myMajor, myTextEditor, Zip, pi, bookCost, lightSwitchOn, superManFly);
 
 // Problem 3) Use the typeof operator to determine the data type of each variable like so.
 	// console.log(typeof myEmail);
 	// console.log(typeof myMajor);
 	// and so on…
 	// Make sure you have at least one string, at least one number, and at least one Boolean.
-
+console.log(typeof myEmailAdd);
+console.log(typeof myMajor);
+console.log(typeof myTextEditor);
+console.log(typeof Zip);
+console.log(typeof pi);
+console.log(typeof bookCost);
+console.log(typeof lightSwitchOn);
+console.log(typeof superManFly);
 
 // Problem 4) Create string variables to store the following string values.
 // Send all variables to the console.
@@ -42,10 +58,16 @@
 	// <a href="#">Some link</a>
 	// JavaScript is "easy" if you are good at it
 
+var s = "The dog's biting my shoes";
+var t = "<a href='#'>Some Link</a>";
+var u = "JavaScript is 'easy' if you are good at it";
+console.log(s, t, u);
 
 // Problem 5) Create an array of topics you would like to learn this semester.
 // Send this array to the console.
 
+var jsTopics = ["Types", "Values", "Variables", "Expressions", "Statements", "Objects", "Arrays", "Classes", "Modules"];
+console.log(jsTopics);
 
 // Problem 6) Send each item of the last array to the console. 
 // If your array variable is myArray then you would write
@@ -53,11 +75,21 @@
 	// console.log(myArray[1])
 	// and so on…
 
+console.log(jsTopics[0]);
+console.log(jsTopics[1]);
+console.log(jsTopics[2]);
+console.log(jsTopics[3]);
+console.log(jsTopics[4]);
+console.log(jsTopics[5]);
+console.log(jsTopics[6]);
+console.log(jsTopics[7]);
+console.log(jsTopics[8]);
 
 // Problem 7) Use the yourArrayVariable.length property to determine the size of your array.
 // Send the result to the console.
 
-
+console.log(jsTopics.length);
+          
 // Problem 8) Create a function named "clickMe".
 // When the user clicks the button with the ID of "clickMe" send the string 
 // "Leave me alone!" to the console.
