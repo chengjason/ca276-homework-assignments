0f9e3cd40694b52ac03d58ad97e991c9853ec70d
chengjason <cadillacboy09@gmail.com>
2013-10-03 16:38:29 -0400

    Submitting hw2

---
+++ hw3.js/ca276-homework-assignments-master/hw2.js
@@ -0,0 +1,116 @@
+/*
+ * Please follow the instructions carefully.
+ * 
+ * When a problem says "send 'foo' to the console that means you would write
+ * 
+ * 	console.log('foo');
+ * 
+ * I may also say "send the variable x to the console" in which case you'd write
+ * 
+ * 	console.log(x)
+ * 
+ * Note: If you have multiple variables x, y, z you can send all of them to the console like so
+ * 
+ * 	console.log(x, y, z)
+ */
+
+// Problem 1) Using JavaScript comments, write your name and today's date below
+//Write my name 
+console.log('Jason Cheng');
+//Write today's date
+console.log('October 3, 2013');
+
+
+// Problem 2) Create separate variables to store the following. 
+// Send all variables to the console.
+	// Your e-mail address
+	// Your major
+	// Your favorite text editor
+	// A zip code
+	// The value of pi up to 5 decimal places (use Google to find the value)
+	// The total cost of a 10 dollar book plus 5% sales tax
+	// Whether or not the light switch is on
+	// Whether or not Superman can fly
+var myEmailAdd = 'jcheng19@montgomerycollege.edu';
+var myMajor = 'Computer Applications'; 
+var myTextEditor = 'Dreamweaver CS6';
+var	zip = '20850';
+var pi = 3.14159;
+var x = 10, bookCost = x*(1 + 0.05);
+var lightSwitchOn = true, lightSwitchOff = false;
+var superManFly = true, superManCannotFly = false;
+console.log(myEmailAdd);
+console.log(myMajor);
+console.log(myTextEditor);
+console.log(zip);
+console.log(pi);
+console.log(bookCost);
+console.log(lightSwitchOn);
+console.log(superManFly);
+
+// Problem 3) Use the typeof operator to determine the data type of each variable like so.
+	// console.log(typeof myEmail);
+	// console.log(typeof myMajor);
+	// and so on…
+	// Make sure you have at least one string, at least one number, and at least one Boolean.
+console.log(typeof myEmailAdd);
+console.log(typeof myMajor);
+console.log(typeof myTextEditor);
+console.log(typeof zip);
+console.log(typeof pi);
+console.log(typeof bookCost);
+console.log(typeof lightSwitchOn);
+console.log(typeof superManFly);
+
+// Problem 4) Create string variables to store the following string values.
+// Send all variables to the console.
+	// The dog's biting my shoes
+	// <a href="#">Some link</a>
+	// JavaScript is "easy" if you are good at it
+var a = "The dog's biting my shoes";
+var b = '<a href="#">Some link</a>';
+var c = 'JavaScript is "easy" if you are good at it';
+console.log(a);
+console.log(b);
+console.log(c);
+
+// Problem 5) Create an array of topics you would like to learn this semester.
+// Send this array to the console.
+var jsTopics = ["Types", "Variables", "Functions", "Objects", "Arrays", "Classes", "Expressions", "Modules"];
+console.log(jsTopics);
+
+// Problem 6) Send each item of the last array to the console. 
+// If your array variable is myArray then you would write
+	// console.log(myArray[0])
+	// console.log(myArray[1])
+	// and so on…
+console.log(jsTopics[0]);
+console.log(jsTopics[1]);
+console.log(jsTopics[2]);
+console.log(jsTopics[3]);
+console.log(jsTopics[4]);
+console.log(jsTopics[5]);
+console.log(jsTopics[6]);
+console.log(jsTopics[7]);
+console.log(jsTopics[8]);
+
+// Problem 7) Use the yourArrayVariable.length property to determine the size of your array.
+// Send the result to the console.
+console.log(jsTopics.length);
+
+// Problem 8) Create a function named "clickMe".
+// When the user clicks the button with the ID of "clickMe" send the string 
+// "Leave me alone!" to the console.
+	// hint: Use document.getElementById to get a reference to the button in
+	// a similar way to how we did it in class for the currency conversion
+	// calculator.
+<p id ="clickMe">
+<button  onclick ="clickMe()">Click Me</button>
+</p>
+
+function clickMe(){
+	document.getElementById("clickMe").innerHTML="Leave me alone!";
+}
+
+
+	
