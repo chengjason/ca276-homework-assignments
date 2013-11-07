a5cb5e76ba80cb12e007d52c90c5734a2c5abced
Jose Galdamez <mcstudent@jose-galdamezs-computer.local>
2013-11-07 00:30:01 -0500

    Homework 4 corrections

--- hw4.js
+++ hw4.js
@@ -27,10 +27,7 @@
 
 
 var athlete1 = {};
-// athlete.foo = "bar"
-// athlete.method = function () {
-// 
-// }
+
 athlete1.name = "Jason Cheng";
 athlete1.height = "5.7 ft";
 athlete1.age = "23";
@@ -38,12 +35,14 @@ athlete1.country = "USA";
 athlete1.medals = ["Silver", "Bronze"];
 athlete1.isOlympian = false;
 
-athlete1.introduce = function(){
-    return "Hello! My name is " + athlete1.name + " and I am from " + athlete1.country + ".";
+athlete1.introduce = function() {
+	return "Hello! My name is " + this.name + " and I am from " + this.country + ".";
 }
 
-athlete1.addMedal = athlete1.medals.push("Gold");
-
+//JG: This is supposed to be a function
+athlete1.addMedal = function(medal) {
+	this.medals.push(medal);
+}
 
 
 // Problem 2
@@ -51,10 +50,6 @@ athlete1.addMedal = athlete1.medals.push("Gold");
 
 
 var athlete2 = {};
-// athlete['foo'] = "bar"
-// athlete['method'] = function () {
-// 
-// }
 
 athlete2['name'] = "Jason Cheng";
 athlete2['height'] = "5.7 ft";
@@ -63,12 +58,13 @@ athlete2['country'] = "USA";
 athlete2['medals'] = ["Silver", "Bronze"];
 athlete2['isOlympian'] = false;
 
-athlete2['introduce'] = function(){
-    return "Hello! My name is " + athlete2['name'] + " and I am from " + athlete2['country'] + ".";
+athlete2['introduce'] = function() {
+	return "Hello! My name is " + this['name'] + " and I am from " + this['country'] + ".";
 }
 
-athlete2['addMedal'] = athlete2['medals'].push("Gold");
-
+athlete2['addMedal'] = function(medal) {
+	this['medals'].push(medal);
+}
 
 
 
@@ -79,39 +75,52 @@ athlete2['addMedal'] = athlete2['medals'].push("Gold");
 var athlete3 = {
 	// set key/value pairs inside here
 	name: "Jason Cheng",
-   height: "5.7 ft",
-   age: "23",
-   country: "USA",
-   medals: ["Silver", "Bronze"],
-   isOlympian: false,
-    
-
-   introduce: function(){
-     return "Hello! My name is " + this.name + " and I am from " + this.country + ".";
-    }
- }
+	height: "5.7 ft",
+	age: "23",
+	country: "USA",
+	medals: ["Silver", "Bronze"],
+	isOlympian: false,
+	introduce: function() {
+		return "Hello! My name is " + this.name + " and I am from " + this.country + ".";
+	},
+	addMedal: function(medal) {
+		this['medals'].push(medal);
+	}
+}
 
 athlete3.addMedal = athlete3.medals.push("Gold");
 
 // Problem 4
 // Create a prototype for athelete. See below for examples:
 // https://github.com/galdamez/ca276-fall2013/blob/master/week8/prototypes.html
-function athlete(name, age, height, country, medals){
-      this.name = name;
-      this.age = age;
-      this.height = height;
-      this.country = country;
-      this.medals = medals;
 
-      this.present = function(){
-         console.log( "Hello! My name is " + this.name + ".  I am " + this.age + " years old.");
-      }
+//JG: Use a capital letter when defining prototypes
+function Athlete(name, age, height, country, medals, isOlympian) {
+	this.name = name;
+	this.age = age;
+	this.height = height;
+	this.country = country;
+	this.medals = medals;
+	//JG: This property was missing
+	this.isOlympian = isOlympian;
+
+	//JG: Incorrect function name
+	this.introduce = function() {
+		return "Hello! My name is " + this.name + ".  I am from " + this.country;
+	}
+
+	//JG: This was missing
+	this.addMedal = function(medal) {
+		this['medals'].push(medal);
+	}
 }
-var p1 = new athlete("Jarvis Hayes", "23", "7.6 ft", "USA", "Bronze");
-var p2 = new athlete("Chucky Atkins", "31", "6.6 ft", "USA", "Gold");
 
-p1.present();
-p2.present();
+var p1 = new Athlete("Jarvis Hayes", "23", "7.6 ft", "USA", "Bronze");
+var p2 = new Athlete("Chucky Atkins", "31", "6.6 ft", "USA", "Gold");
+
+//JG: Use console.log actually show the value
+console.log(p1.introduce());
+console.log(p2.introduce());
 
 
 
@@ -141,20 +150,47 @@ p2.present();
 // Method below returns 'Splash splash splash!'
 // 
 // s1.swim();
-function golfPlayer(name, age, country, gender, professional, record){
-     this.name = name;
-     this.age = age;
-     this.country = country;
-     this.gender = gender;
-     this.professional = professional;
-	 this.bestRecord = record;
+function GolfPlayer(name, age, country, gender, professional, record) {
+	this.name = name;
+	this.age = age;
+	this.country = country;
+	this.gender = gender;
+	this.professional = professional;
+	this.bestRecord = record;
 
-     this.playerInfo = function(){
-         console.log("Player's Information" + "\n" + "Name: " + this.name +  "\n" + "Age: " + this.age + "\n" + "Country of Origin: " + this.country + "\n" + "Gender: " 
-		 + this.gender + "\n" + "Professional: " +  this.professional + "\n" + "Best Record: " + this.bestRecord + "\n");
-      }
-     
+	this.playerInfo = function() {
+		//JG: Try to return values instead of using console.log
+		//JG: I'm using a trick with arrays to split the string into multiple lines
+		return [
+			"Player's Information",
+			"\n",
+			"Name: ",
+			this.name,
+			"\n",
+			"Age: ",
+			this.age,
+			"\n",
+			"Country of Origin: ",
+			this.country,
+			"\n",
+			"Gender: ",
+			this.gender,
+			"\n",
+			"Professional: ",
+			this.professional,
+			"\n",
+			"Best Record: ",
+			this.bestRecord,
+			"\n"
+		].join('');
+	}
 }
 
-var p1 = new golfPlayer("Tiger Woods", "38", "USA", "Male", "true", "-13");
-p1.playerInfo();
+//JG: You need these two lines to get inheritance working
+GolfPlayer.prototype = new Athlete();
+GolfPlayer.prototype.constructor = Athlete;
+
+//JG: You already used "var" on p1 up above
+//JG: I'm renaming this to p3
+var p3 = new GolfPlayer("Tiger Woods", "38", "USA", "Male", "true", "-13");
+console.log(p3.playerInfo());
